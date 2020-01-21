cc9bf88b635c764625cd97b54bef94372fd28df7
linjiang <linjiang.me@gmail.com>
2020-01-21 16:15:32 +0800

    解决api文件无法被compile的问题

--- buildSrc/src/main/groovy/io/letusplay/plugin/IncludeAPIPlugin.groovy
+++ buildSrc/src/main/groovy/io/letusplay/plugin/IncludeAPIPlugin.groovy
@@ -7,6 +7,7 @@ import org.gradle.api.artifacts.Configuration
 class IncludeAPIPlugin implements Plugin<Project> {
 
   List<String> apiSdks = new ArrayList<>()
+  List<String> copyFiles = new ArrayList<>()
 
   @Override
   void apply(Project project) {
@@ -17,6 +18,42 @@ class IncludeAPIPlugin implements Plugin<Project> {
     configurations.visible = true
 
     project.afterEvaluate {
+      // 两种解决 compile-task无法编译*.*api文件 方案：
+      // 1. 直接动态依赖module自己暴露的 sdk-api
+//      project.parent.allprojects { p ->
+//        if (p.name == "${project.name}-api") {
+//          project.dependencies.add("api", p)
+//        }
+//      }
+
+      // 2. 在编译前将module里的api文件还原为java/kt，编译后删除
+      if (project.plugins.hasPlugin("com.android.application")) {
+        copyedFiles.clear()
+        project.parent.allprojects { Project p ->
+          p.fileTree(p.projectDir).include("**/*api")?.each { File file ->
+            println("find .api in ${p.name}: ${file}")
+            if (file.name.endsWith(".japi")) {
+              def dest = new File(file.absolutePath.replace(".japi", ".java"))
+              if (dest.exists()) dest.delete()
+              dest << file.text
+              copyFiles.add(dest)
+            } else if (file.name.endsWith(".kapi")) {
+              def dest = new File(file.absolutePath.replace(".kapi", ".kt"))
+              if (dest.exists()) dest.delete()
+              dest << file.text
+              copyFiles.add(dest)
+            }
+          }
+        }
+        project.parent.gradle.buildFinished {
+          println("buildFinished: ${project.name}")
+          copyFiles.forEach { File file ->
+            println("delete tmp file: ${file}")
+            file.delete()
+          }
+        }
+      }
+
       configurations.allDependencies.all {
         dep ->
           String targetName = "${dep.name}-api"
